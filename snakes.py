3cc16f18edace5aaec5aef9c10cc9520ddeab2ad
Andrey <koccep@yandex.ru>
2020-11-12 17:13:48 +0300

    Fixes painter class

--- snakes.py
+++ snakes.py
@@ -4,13 +4,13 @@ import string
 
 class Snake:
 
-    def __init__(self, foods, window=None, ai=0, srow=None, scol=None, direction=None, color=1, size=None, fill=None):
+    def __init__(self, foods, board=None, ai=0, srow=None, scol=None, direction=None, color=1, size=None, fill=None):
         self.size = size if size is not None  else random.randint(2,20)
         self.fill = fill if fill is not None else random.choice(string.ascii_uppercase)
         self.direction = direction if direction is not None  else random.choice(list(dirdict.values()))
         self.color = color
         self.ai = ai
-        self.window = window if window is not None else screen
+        self.board = board if board is not None else screen
         self.foods = foods
         self.collision = 0
         self.needgrow = 0
@@ -18,19 +18,15 @@ class Snake:
         self.reset(srow,scol)
 
     def reset(self,srow=None,scol=None):
-        brows, bcols = self.window.getbegyx()
-        frows, fcols = self.window.getmaxyx()
-        frows = frows - 1
-        fcols = fcols - 1
         self.body = [[self.fill,0,0] for i in range(self.size)]
-        self.body[0][1] = srow if srow is not None else random.randint(brows,frows)
-        self.body[0][2] = scol if scol is not None else random.randint(bcols,fcols)
+        self.body[0][1] = srow if srow is not None else random.randint(self.board.miny,self.board.maxy - 1)
+        self.body[0][2] = scol if scol is not None else random.randint(self.board.minx,self.board.maxx - 1)
         self.collision = 0
 
     def death(self):
         self.score -= 100
         for i in range(0,len(self.body)-1):
-            self.window.drawch(self.body[i][1], self.body[i][2], " ")
+            self.board.drawch(self.body[i][1], self.body[i][2], " ")
         self.reset()
 
     def move(self):
@@ -50,45 +46,40 @@ class Snake:
                     self.direction = random.choice([0,2])
         #Если ручное управление
 
-        brows, bcols = self.window.getbegyx()
-        frows, fcols = self.window.getmaxyx()
-        frows = frows - 1
-        fcols = fcols - 1
-
         #Совершаем движение на один шаг в выбранном направлении
         if self.direction == 0:
             row += 1
-            if row >= frows:
-                row = brows
+            if row >= self.board.maxy -1:
+                row = self.board.miny
         elif self.direction == 2:
             row -= 1
-            if row <= brows:
-                row = frows
+            if row <= self.board.miny:
+                row = self.board.maxy
         elif self.direction == 1:
             col -= 1
-            if col <= bcols:
-                col = fcols
+            if col <= self.board.minx:
+                col = self.board.maxx
         elif self.direction == 3:
             col += 1
-            if col >= fcols:
-                col = bcols
+            if col >= self.board.maxx - 1:
+                col = self.board.minx
 
         #Проверяет нет ли коллизии
         if self.check_collision(row,col) == 0 and self.collision != 1:
             self.body.insert(0, [self.fill,row,col])
-            self.window.drawch(row, col, self.body[0][0])
+            self.board.drawch(row, col, self.body[0][0])
             self.score += 1
             self.catchfood(row,col)
             if not self.needgrow:
                 self.body.pop(len(self.body) - 1)
-                self.window.drawch(self.body[-1][1], self.body[-1][2], " ")
+                self.board.drawch(self.body[-1][1], self.body[-1][2], " ")
             else:
                 self.needgrow = 0
         else:
            self.death()
 
     def check_collision(self,row,col):
-        if self.window.checkchar(row,col) in list(string.ascii_uppercase):
+        if self.board.checkchar(row,col) in list(string.ascii_uppercase):
             return 1
         else:
             return 0
@@ -102,54 +93,45 @@ class Snake:
 
 class Food:
     
-    def __init__(self, window=None, view=None, price=None):
+    def __init__(self, board=None, view=None, price=None):
         self.price = price if price is not None else 10
         self.view = view if view is not None else random.choice(string.punctuation)
-        self.window = window if window is not None else screen
+        self.board = board if board is not None else screen
         self.row = 0
         self.col = 0
         self.spawn()
 
     def spawn(self):
-        brows, bcols = self.window.getbegyx()
-        frows, fcols = self.window.getmaxyx()
-        frows = frows - 1
-        fcols = fcols - 1
-        
         while True:
-            self.row = random.randint(brows,frows)
-            self.col = random.randint(bcols,fcols)
-            if self.window.checkchar(self.row,self.col) == " ":
+            self.row = random.randint(self.board.miny,self.board.maxy - 1)
+            self.col = random.randint(self.board.minx,self.board.maxx - 1)
+            if self.board.checkchar(self.row,self.col) == " ":
                 break
     
     def draw(self):
-        self.window.drawch(self.row, self.col, self.view)
+        self.board.drawch(self.row, self.col, self.view)
 
-class Painter:
+class Board:
 
     def __init__(self,rows,cols,srow,scol):
-        self.window = curses.newwin(rows,cols,srow,scol)
+        self.board = curses.newwin(rows,cols,srow,scol)
+        self.miny, self.minx = self.board.getbegyx()
+        self.maxy, self.maxx = self.board.getmaxyx()
     
     def drawwin(self):
-        self.window.box(curses.A_VERTICAL,curses.A_HORIZONTAL)
+        self.board.box(curses.A_VERTICAL,curses.A_HORIZONTAL)
 
     def drawch(self,row,col,ch=" ",color=1):
-        self.window.addch(row, col, ch)
+        self.board.addch(row, col, ch)
 
     def drawstr(self,row,col,st=" ",color=1):
-        self.window.addstr(row, col, st)
+        self.board.addstr(row, col, st)
     
     def refresh(self):
-        self.window.refresh()
+        self.board.refresh()
 
     def checkchar(self,row,col):
-        return chr((self.window.inch(row,col)) & 0xFF)
-
-    def getbegyx(self):
-        return self.window.getbegyx()
-
-    def getmaxyx(self):
-        return self.window.getmaxyx()
+        return chr((self.board.inch(row,col)) & 0xFF)
 
 def key_pressed(char):
     if char == ord("q"): return -1
@@ -159,7 +141,7 @@ def key_pressed(char):
     elif char == ord("d") or char == ord("D") or char == curses.KEY_RIGHT: return 3
 
 #Constant
-SNAKE_NUMBER = 6
+SNAKE_NUMBER = 1
 FOOD_NUMBER = 6
 
 #Init values
@@ -174,8 +156,8 @@ screen.nodelay(True)
 screen.erase()
 
 #Create window
-gameboard = Painter(30,90,0,0)
-scoreboard = Painter(20,30,0,93)
+gameboard = Board(30,90,0,0)
+scoreboard = Board(20,30,0,93)
 
 scoreboard.drawwin()
 scoreboard.drawstr(0,9,"SCORE BOARD:")
